88358662d2126b5e6c2997611cb1e363c1f4f7ba
Yura <resoul@bk.ru>
2025-08-31 00:50:18 +0300

    Update Configurator.swift

--- Sources/AtomicKit/Container/Configurator.swift
+++ Sources/AtomicKit/Container/Configurator.swift
@@ -1,44 +1,15 @@
 import Foundation
 
-public protocol Injectable {
-    static func create(container: Container) -> Self
-}
-
-public protocol AutoInjectable: Injectable {
-    init()
-}
-
-extension AutoInjectable {
-    public static func inject(container: Container) -> Self {
-        return Self()
-    }
-}
-
 public protocol Module {
     func configure(container: Container)
 }
 
-public protocol ServiceModule: Module {}
-public protocol DataModule: Module {}
-public protocol DomainModule: Module {}
-public protocol PresentationModule: Module {}
-public protocol CoordinatorModule: Module {}
-
 public final class Configurator {
     public static func configure(
-        services: [ServiceModule] = [],
-        data: [DataModule] = [],
-        domains: [DomainModule] = [],
-        presentation: [PresentationModule] = [],
-        coordinators: [CoordinatorModule] = [],
+        modules: [Module] = [],
         container: Container = SafeContainer.shared
     ) {
-        // Sorting: Services → Data → Domain → Presentation → Coordinators
-        services.forEach { $0.configure(container: container) }
-        data.forEach { $0.configure(container: container) }
-        domains.forEach { $0.configure(container: container) }
-        presentation.forEach { $0.configure(container: container) }
-        coordinators.forEach { $0.configure(container: container) }
+        modules.forEach { $0.configure(container: container) }
     }
 }
 
